828172b17ffad703639dcd3a278e0d430f90b837
infiniteazuree <christianviali@yahoo.com>
2024-08-04 15:50:47 +0700

    sitemap test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/sitemap', function () {
+
+    // buat halaman index utama, disini saya contohkan ada 4 halaman, home, course, event dan artikel
+    $sitemap = Sitemap::create()
+        ->add(Url::create('/'))
+        ->add(Url::create('/course'))
+        ->add(Url::create('/artikel'));
+
+    $sitemap->writeToFile(public_path('sitemap.xml'));
+
+    return "Sitemap berhasil diperbarui, <a href='/sitemap.xml'>Lihat sitemap</a>";
 });
